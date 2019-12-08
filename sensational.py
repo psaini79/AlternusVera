860400a60ceb282982b3bd93cc2bf35c49d8db55
Paramdeep Saini <paramdeep.saini@sjsu.edu>
2019-12-08 08:09:42 +0000

    change

--- sensational.py
+++ sensational.py
@@ -7,11 +7,104 @@ import random
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 from nltk.tokenize import word_tokenize
 
+sensationalist_phrases = [
+
+	# From Analyst's Desktop Binder of Homeland Security https://www.scribd.com/doc/82701103/Analyst-Desktop-Binder-REDACTED
+	
+	'Assassination',
+	'Attack',
+	'Domestic security',
+	'Law enforcement',
+	'Disaster',
+	'National preparedness',
+	'Response',
+	'Recovery',
+	'Emergency response',
+	'First responder',
+	'Militia',
+	'Shooting',
+	'Evacuation',
+	'Hostage',
+	'Explosion',
+	'Organized crime',
+	'Gangs',
+	'National security',
+	'State of emergency',
+	'Security breach',
+	'Threat',
+	'Standoff',
+	'Lockdown',
+	'Bomb',
+	'Riot',
+	'Emergency Landing',
+	'Incident',
+	'Suspicious',
+	'Nuclear threat',
+	'Hazardous',
+	'Infection',
+	'Outbreak',
+	'Contamination',
+	'Terror',
+	'Epidemic',
+	'Critical Infrastructure',
+	'National infrastructure',
+	'Transportation security',
+	'Grid',
+	'Outage',
+	'Disruption',
+	'Violence',
+	'Drug cartel',
+	'Narcotics',
+	'Shootout',
+	'Trafficking',
+	'Kidnap',
+	'Illegal',
+	'Smuggling', 
+	'Al Qaeda',
+	'Terror attack',
+	'Weapon',
+	'Improvised explosive device',
+	'Suicide bomber',
+	'Suicide attack',
+	'Hurricane',
+	'Tornado',
+	'Tsunami',
+	'Earthquake',
+	'Tremor',
+	'Flood',
+	'Storm',
+	'Extreme weather',
+	'Forest fire',
+	'Ice',
+	'Stranded',
+	'Wildfire',
+	'Avalanche',
+	'Blizzard',
+	'Lightening',
+	'Emergency Broadcast System',
+	'Cyber Security',
+	'DDOS',
+	'Denial of service',
+	'Malware',
+	'Phishing',
+	'Cyber attack',
+	'Cyber terror'
+]
+
+def sensphrasedetect(str):
+    sum =0
+    for x in sensationalist_phrases:
+        if x.lower() in str.lower():
+            sum+=1
+    return sum
+
 def prediction(xtest, ytest):
     pickleModel = "/content/gdrive/My Drive/Drifters/Models/sensational_Model.pkl"
     pickle_in = open(pickleModel, "rb")
     loadData = pickle.load(pickle_in)
     return np.mean(loadData.predict(xtest) == ytest)
+
+
 
 def processFakeNews(fnews):
     count = lambda l1,l2: sum([1 for x in l1 if x in l2])
